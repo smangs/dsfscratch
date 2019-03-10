ff674f01c8ecebac98c4d02ca82887637bc27e2b
Sascha Mangs <sascha.mangs@yale.edu>
2019-03-10 18:40:22 -0400

    Initial setup

---
+++ linear_algebra/4_1.py
@@ -0,0 +1,16 @@
+from typing import List
+
+Vector = List[float]
+
+height_weight_age = [70,
+                     170,
+                     40]
+
+grades = [95,
+          80,
+          75,
+          62]
+
+print(height_weight_age)
+
+print(grades)
